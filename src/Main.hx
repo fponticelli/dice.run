4843490a94a0085116125742cc62c12fa5368849
Franco Ponticelli <franco.ponticelli@gmail.com>
2017-02-22 08:37:01 -0700

    more basic stuff

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,16 @@
-class Main {
-  public static function main() {
-    trace("hello world");
+import react.React;
+import react.ReactComponent;
+import react.ReactDOM;
+import js.Browser;
+
+class Main extends ReactComponent {
+
+  static public function main() {
+    ReactDOM.render(React.createElement(Main), Browser.document.getElementById('main'));
+  }
+
+  override function render() {
+    var cname = 'foo';
+    return React.createElement('div', {className:cname}, ["content goes here"]);
   }
 }
